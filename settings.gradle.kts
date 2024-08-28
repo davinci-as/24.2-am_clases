1ad62b5199a385640d3a4f2529dea420b3e1cd64
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-08-27 22:08:58 -0300

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "24.2-clase_03"
+include(":app")
