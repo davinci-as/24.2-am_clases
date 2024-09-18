62dc6b242f0f81293e37a95c6b9612fd5006254a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-09-17 22:27:45 -0300

    feat: mostrar el extra en el activity next

--- app/src/main/java/ar/edu/davinci/a242_clase_03/NextActivity.java
+++ app/src/main/java/ar/edu/davinci/a242_clase_03/NextActivity.java
@@ -2,7 +2,11 @@ package ar.edu.davinci.a242_clase_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.logging.Logger;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        Intent intent = getIntent();
+        TextView secondText = findViewById(R.id.secondText);
+
+        if(intent.hasExtra("newText")) {
+            String newText = intent.getStringExtra("newText");
+            secondText.setText(newText);
+        } else {
+            secondText.setText(getText(R.string.next_text));
+        }
     }
 }
