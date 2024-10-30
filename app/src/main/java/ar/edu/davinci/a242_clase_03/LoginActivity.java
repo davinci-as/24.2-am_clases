e136f17b828690e3086333daf95131b11e0f926d
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-10-29 22:11:15 -0300

    feat: login with email and password

--- app/src/main/java/ar/edu/davinci/a242_clase_03/LoginActivity.java
+++ app/src/main/java/ar/edu/davinci/a242_clase_03/LoginActivity.java
@@ -1,14 +1,46 @@
 package ar.edu.davinci.a242_clase_03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void onClick(View view) {
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.i("firebase-log", mAuth.getCurrentUser().getEmail());
+                        } else {
+                            Log.i("firebase-log", task.getException().toString());
+                        }
+                    }
+                });
     }
 }
