4a7428bf2ecb70aeb1d95085002bc77e0c990b1a
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2024-11-05 22:28:15 -0300

    feat: add firestore db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation("com.google.firebase:firebase-firestore")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/ar/edu/davinci/a242_clase_03/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a242_clase_03/MainActivity.java
@@ -1,18 +1,30 @@
 package ar.edu.davinci.a242_clase_03;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     public void download() {
         ImageView imageView = findViewById(R.id.imageView);
@@ -20,15 +32,42 @@ public class MainActivity extends AppCompatActivity {
         downloader.execute(getString(R.string.panda_image));
     }
 
+    public void getCollection () {
+
+    }
+
+    public void addCharacter () {
+        Map<String, Object> character = new HashMap<>();
+        character.put("name", "Ada");
+        character.put("status", "Dead");
+        character.put("specie", "Human");
+
+        db.collection("characters").add(character)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("firebase-firestore", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("firebase-firestore", "Error adding document", e);
+                    }
+                });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             setContentView(R.layout.activity_main);
             download();
+            //addCharacter();
         } else {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
